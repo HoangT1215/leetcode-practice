7175869a00871aa854f2ff87f91e17939558ca3d
Hoang Nguyen <hoangt1215@gmail.com>
2026-08-27 02:31:16 -0400

    Add unique BST dynamic programming solution

---
+++ dynamic-programming/0096_unique_binary_search_trees.py
@@ -0,0 +1,27 @@
+"""LeetCode 96: Unique Binary Search Trees.
+
+Problem: https://leetcode.com/problems/unique-binary-search-trees/
+
+Time complexity: O(n^2)
+Space complexity: O(n)
+"""
+
+
+class Solution:
+    def numTrees(self, n: int) -> int:
+        if n <= 1:
+            return 1
+
+        # The direct recursive recurrence is correct but takes exponential
+        # time when its overlapping subproblems are not memoized.
+
+        # dp[j] is the number of unique BSTs containing j ordered nodes.
+        dp = [0] * (n + 1)
+        dp[0], dp[1] = 1, 1
+
+        for number_of_nodes in range(2, n + 1):
+            for left_size in range(number_of_nodes):
+                right_size = number_of_nodes - left_size - 1
+                dp[number_of_nodes] += dp[left_size] * dp[right_size]
+
+        return dp[n]
